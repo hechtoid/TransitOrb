e5dc930bdaf67020daf6a95a98697b864c843fb9
hechtoid <hechtsp@gmail.com>
2020-02-15 19:09:22 -0800

    Updates

---
+++ precache-manifest.4c2383f17c8412f6dde2f4bd81f9c00f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373c50c198e9872a1085b9b3822297c8",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "16be0002a6b16b5b97ee",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d47993060d4fa50951c6",
+    "url": "/react-gh-pages/static/js/2.8203cfe9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-gh-pages/static/js/2.8203cfe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16be0002a6b16b5b97ee",
+    "url": "/react-gh-pages/static/js/main.9136ba6c.chunk.js"
+  },
+  {
+    "revision": "5cf2807551b5cb2c46ca",
+    "url": "/react-gh-pages/static/js/runtime-main.8a3e7cc9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
