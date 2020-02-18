41ffd247dc70636a335ca5eb78c8b80668b1efd8
hechtoid <hechtsp@gmail.com>
2020-02-18 12:45:13 -0800

    predeploy

--- src/components/transit.js
+++ src/components/transit.js
@@ -11,12 +11,23 @@ function Transit() {
   return (
     <div className="transit-master">
       <div className="transit-switcher">
-        <div className="busemoji">🚌</div>
+        <div className="busemoji">
+          <a href="https://github.com/hechtoid/react511" target="_blank">
+            🚌
+          </a>
+        </div>
         <div id='marin' className={pane === 'marin' ? 'transit-switch-on' : 'transit-switch-off'} onClick={() => setPane('marin')}>MARIN</div>
         <div className={pane === 'frisco' ? 'transit-switch-on' : 'transit-switch-off'} onClick={() => setPane('frisco')}>FRISCO</div>
         <div className={pane === 'user' ? 'transit-switch-on' : 'transit-switch-off'} onClick={() => setPane('user')}>USER</div>
-
+        <div className="five-eleven">
+          <a href="https://511.org/open-data/transit" target="_blank">
+          <img className="five-eleven" src="http://proxy-prod.511.org/assets/img/branding/511_original_web.png">
+          </img>
+          </a>
+        </div>
       </div>
+
+
     <div className="transit">
         <div className={pane === 'marin' ? 'transit-on' : 'transit-off'} >
           <Sansome />
