bae0c60bde3cbc5e735767807efa72392026fa96
hechtoid <hechtsp@gmail.com>
2020-02-24 13:07:24 -0800

    Updates

---
+++ precache-manifest.b0e2d4fbbd03a7b3a28daaa01231c431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b034c69d8ab973e28f7633891a67d5",
+    "url": "/transitYourself/index.html"
+  },
+  {
+    "revision": "eb71e63eb20b41ca8b3c",
+    "url": "/transitYourself/static/css/main.7c6c52d9.chunk.css"
+  },
+  {
+    "revision": "1ffca77f7b84df23f64f",
+    "url": "/transitYourself/static/js/2.a935ace9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/transitYourself/static/js/2.a935ace9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb71e63eb20b41ca8b3c",
+    "url": "/transitYourself/static/js/main.c0e7697a.chunk.js"
+  },
+  {
+    "revision": "13751949aa3e0a9946b5",
+    "url": "/transitYourself/static/js/runtime-main.a28ec231.js"
+  }
+]);
