762aa3d0c676f458203df1ce8e5424f6bab0cce9
hechtoid <hechtsp@gmail.com>
2020-03-04 14:19:26 -0800

    predeploy

--- src/components/vehicular.js
+++ src/components/vehicular.js
@@ -2,7 +2,6 @@ import React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-
 class Vehicular extends React.Component {
     constructor(props) {
         super(props);
