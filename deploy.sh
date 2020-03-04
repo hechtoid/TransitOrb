b39c560e762854efd24aeca743053d808180d332
hechtoid <hechtsp@gmail.com>
2020-03-04 12:44:51 -0800

    map test

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 cd /home/www/transitYourself/
 
 npm run build &&
 cp build/index.html build/404.html &&
 git add . &&
 git commit -m 'predeploy' &&
-git push &&
 npx gh-pages -d build &&
+git push &&
 
 exit
