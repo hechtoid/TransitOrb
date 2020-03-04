ea10720f2294f58e551451cf9f07256242cae0ee
hechtoid <hechtsp@gmail.com>
2020-03-04 01:35:59 -0800

    predeploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 cd /home/www/transitYourself/
 
+npm run build &&
+cp build/index.html build/404.html &&
 git add . &&
 git commit -m 'predeploy' &&
-npm run build &&
-cp build/index.html build/404.html
 git push &&
 npx gh-pages -d build &&
 
