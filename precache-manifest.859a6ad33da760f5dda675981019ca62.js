0d046b597323dc1b64e4e1d8375f6d860801a040
hechtoid <hechtsp@gmail.com>
2020-03-05 17:40:04 -0800

    Updates

--- precache-manifest.859a6ad33da760f5dda675981019ca62.js
+++ precache-manifest.859a6ad33da760f5dda675981019ca62.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "88ed63d9bb3f018ae7442255074fac0f",
+    "revision": "c31db5100874e956ecec2125af775da5",
     "url": "/transitYourself/index.html"
   },
   {
-    "revision": "c7889ab538ca6fe5de0c",
-    "url": "/transitYourself/static/css/main.5ea3f6a8.chunk.css"
+    "revision": "d493639bb57f32061658",
+    "url": "/transitYourself/static/css/main.9045e5e3.chunk.css"
   },
   {
     "revision": "6bd66dd56328dd12d942",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/transitYourself/static/js/2.3f679c30.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c7889ab538ca6fe5de0c",
-    "url": "/transitYourself/static/js/main.b12e5450.chunk.js"
+    "revision": "d493639bb57f32061658",
+    "url": "/transitYourself/static/js/main.9871af6d.chunk.js"
   },
   {
     "revision": "13751949aa3e0a9946b5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/transitYourself/precache-manifest.03decb04edf36fa04adb66b663745af4.js"
+  "/transitYourself/precache-manifest.859a6ad33da760f5dda675981019ca62.js"
 );
 
 self.addEventListener('message', (event) => {
