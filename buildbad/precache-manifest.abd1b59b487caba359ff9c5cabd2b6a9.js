ae290709e18130c38502044e510ecb426842b8a9
hechtoid <hechtsp@gmail.com>
2020-03-06 11:15:13 -0800

    github ate my tabssZ

---
+++ buildbad/precache-manifest.abd1b59b487caba359ff9c5cabd2b6a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5beb1e1b6a077216a504633c9df7d3c",
+    "url": "/transitYourself/index.html"
+  },
+  {
+    "revision": "e1a0926a9babad962a07",
+    "url": "/transitYourself/static/css/main.a15d33e7.chunk.css"
+  },
+  {
+    "revision": "62f5888eb78424760623",
+    "url": "/transitYourself/static/js/2.4d549890.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/transitYourself/static/js/2.4d549890.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1a0926a9babad962a07",
+    "url": "/transitYourself/static/js/main.f9fc344c.chunk.js"
+  },
+  {
+    "revision": "13751949aa3e0a9946b5",
+    "url": "/transitYourself/static/js/runtime-main.a28ec231.js"
+  }
+]);
