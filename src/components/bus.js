acfd0b13582501eb29368b4c56e4f2cb3a6ddf4b
hechtoid <hechtsp@gmail.com>
2020-03-07 05:55:19 -0800

    git caltrain

--- src/components/bus.js
+++ src/components/bus.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
 function Bus(props) {
 if (props.bus.MonitoredVehicleJourney.OperatorRef === "BA") {
         return (
         <div className="bus" id={props.bus.MonitoredVehicleJourney.LineRef} >
                 <div>
                         <span className="bold">
-                        {props.bus.MonitoredVehicleJourney.DestinationName}
+                {props.bus.MonitoredVehicleJourney.DestinationName}
                         </span>  
                         <span className="white">
-                        {'<='} {props.bus.MonitoredVehicleJourney.OriginName}
+                {'<='} {props.bus.MonitoredVehicleJourney.OriginName}
                         </span>
                 </div>
                 <hr></hr>
@@ -40,36 +39,37 @@ if (props.bus.MonitoredVehicleJourney.OperatorRef === "BA") {
                 if (props.bus.MonitoredVehicleJourney.LineRef === 'Bullet') {caltrainLine = 'RD-N'}
         return (
         <div className="bus" id={caltrainLine}>
-                <div className="bus-left">
-                        <div>
-                                <span title="Track Vehicle" className="bold">
-                        {props.bus.MonitoredVehicleJourney.VehicleRef
-                                ? <Link className="gps" to={{pathname: "/vehicular", state: {
-                vehicleNumber: props.bus.MonitoredVehicleJourney.VehicleRef,
-                agency: props.bus.MonitoredVehicleJourney.OperatorRef}}} >
-                                {props.bus.MonitoredVehicleJourney.LineRef}<sup>GPS</sup>
-                                </Link>
-                                : <span className="no-gps">{props.bus.MonitoredVehicleJourney.LineRef}</span>}
-                                </span>=> {props.bus.MonitoredVehicleJourney.DestinationName} 
-                        </div>
-                        {props.bus.MonitoredVehicleJourney.MonitoredCall.ExpectedDepartureTime 
-                        && props.bus.MonitoredVehicleJourney.MonitoredCall.ExpectedDepartureTime 
-                        !== props.bus.MonitoredVehicleJourney.MonitoredCall.AimedArrivalTime
-                        ? <><span className="bold">
-        {new Date(Date.parse(props.bus.MonitoredVehicleJourney.MonitoredCall.ExpectedDepartureTime)).toLocaleTimeString([],{ hour: 'numeric', minute: 'numeric' })}
-                        </span>
-                        <span className="gray">
-        {new Date(Date.parse(props.bus.MonitoredVehicleJourney.MonitoredCall.AimedArrivalTime)).toLocaleTimeString([],{ hour: 'numeric', minute: 'numeric' })}
-                        </span></>
-                        : <span className="bold">
-        {new Date(Date.parse(props.bus.MonitoredVehicleJourney.MonitoredCall.AimedArrivalTime)).toLocaleTimeString([],{ hour: 'numeric', minute: 'numeric' })}
-                        </span>}
+                <div>
+                        <span title="Track Vehicle" className="bold">
+                {props.bus.MonitoredVehicleJourney.VehicleRef
+                        ? <Link className="gps" to={{pathname: "/vehicular", state: {
+        vehicleNumber: props.bus.MonitoredVehicleJourney.VehicleRef,
+        agency: props.bus.MonitoredVehicleJourney.OperatorRef}}} >
+                        {props.bus.MonitoredVehicleJourney.LineRef}<sup>GPS</sup>
+                        </Link>
+                        : <span className="no-gps">{props.bus.MonitoredVehicleJourney.LineRef}</span>}
+                => </span>{props.bus.MonitoredVehicleJourney.DestinationName} 
                 </div>
-                <div className="countdown">
+                <hr></hr>
+                <div>
+                {props.bus.MonitoredVehicleJourney.MonitoredCall.ExpectedDepartureTime 
+                                && props.bus.MonitoredVehicleJourney.MonitoredCall.ExpectedDepartureTime 
+                                !== props.bus.MonitoredVehicleJourney.MonitoredCall.AimedArrivalTime
+                                ? <><span className="gray">
+        {new Date(Date.parse(props.bus.MonitoredVehicleJourney.MonitoredCall.AimedArrivalTime)).toLocaleTimeString([],{ hour: 'numeric', minute: 'numeric' })}
+                                </span>
+                                <span className="bold">
+        {new Date(Date.parse(props.bus.MonitoredVehicleJourney.MonitoredCall.ExpectedDepartureTime)).toLocaleTimeString([],{ hour: 'numeric', minute: 'numeric' })}
+                                </span></>
+                                : <span className="bold">
+        {new Date(Date.parse(props.bus.MonitoredVehicleJourney.MonitoredCall.AimedArrivalTime)).toLocaleTimeString([],{ hour: 'numeric', minute: 'numeric' })}
+                                </span>}
+                        <div className="countdown">
         {props.bus.MonitoredVehicleJourney.MonitoredCall.ExpectedDepartureTime
 ? <div className="min-math">{Math.floor(((new Date(Date.parse(props.bus.MonitoredVehicleJourney.MonitoredCall.ExpectedDepartureTime))-new Date()))/60000)}</div>
 : <div className="min-math">{Math.floor(((new Date(Date.parse(props.bus.MonitoredVehicleJourney.MonitoredCall.AimedArrivalTime))-new Date()))/60000)}</div>}    
-                        <div className="min">min</div>
+                                <div className="min">min</div>
+                        </div>          
                 </div>
         </div>
     )} else {
@@ -77,18 +77,16 @@ if (props.bus.MonitoredVehicleJourney.OperatorRef === "BA") {
         return(
         <div className="bus">
                 <div>
-                        <span>
-                                <span title="Track Vehicle" className="bold">
-                        {props.bus.MonitoredVehicleJourney.VehicleRef
-                                ? <Link className="gps" to={{pathname: "/vehicular", state: {
+                        <span title="Track Vehicle" className="bold">
+                {props.bus.MonitoredVehicleJourney.VehicleRef
+                        ? <Link className="gps" to={{pathname: "/vehicular", state: {
                 vehicleNumber: props.bus.MonitoredVehicleJourney.VehicleRef,
                 agency: props.bus.MonitoredVehicleJourney.OperatorRef}}} >
                                 {props.bus.MonitoredVehicleJourney.LineRef}<sup>GPS</sup>
-                                </Link>
-                                : <span className="no-gps">{props.bus.MonitoredVehicleJourney.LineRef}</span>}
-                                </span>=> <span className="bold">
-                                        {props.bus.MonitoredVehicleJourney.DestinationName}
-                                </span>
+                        </Link>
+                        : <span className="no-gps">{props.bus.MonitoredVehicleJourney.LineRef}</span>}
+                        </span>=> <span className="bold">
+                {props.bus.MonitoredVehicleJourney.DestinationName}
                         </span>
                 </div>
                 <hr></hr>
