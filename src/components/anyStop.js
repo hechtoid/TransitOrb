8e1ba6907591c310d1ae3d4cdd461569d4c3e98d
hechtoid <hechtsp@gmail.com>
2021-01-17 22:07:04 -0800

    remove old anystop component

--- src/components/anyStop.js
+++ src/components/anyStop.js
@@ -1,60 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Bus from './bus'
 
-class AnyStop extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-           buss: []
-        }
-        this.loadBusss = this.loadBusss.bind(this)
-    }
-    componentDidMount() {
-        this.loadBusss()
-    }
-    loadBusss() {
-        axios.get(`https://api.511.org/transit/StopMonitoring?api_key=72939361-85f9-4019-aa55-d62e4e7e2e59&Format=JSON&agency=${this.props.agency}&stopCode=${this.props.stopCode}`)
+function AnyStop(props) {
+    const [buss, setBuss] = useState([])
+    const [error, setError] = useState('')
+    const loadBusss = () => {
+        axios.get(`https://api.511.org/transit/StopMonitoring?api_key=72939361-85f9-4019-aa55-d62e4e7e2e59&Format=JSON&agency=${props.agency}&stopCode=${props.stopCode}`)
             .then(res => {
                 let buss = res.data.ServiceDelivery.StopMonitoringDelivery.MonitoredStopVisit;
-                if (this.props.filterIN){buss = buss.filter( bus => this.props.filterIN.includes(bus.MonitoredVehicleJourney.LineRef))} 
-                if (this.props.filterOUT){buss = buss.filter( bus => !this.props.filterOUT.includes(bus.MonitoredVehicleJourney.LineRef))}
-                if (this.props.limit){buss = buss.slice(0,this.props.limit)}
-                this.setState({ buss });
-            })
+                if (props.filterIN){buss = buss.filter( bus => props.filterIN.includes(bus.MonitoredVehicleJourney.LineRef))} 
+                if (props.filterOUT){buss = buss.filter( bus => !props.filterOUT.includes(bus.MonitoredVehicleJourney.LineRef))}
+                if (props.limit){buss = buss.slice(0,props.limit)}
+                setBuss(buss);
+            }).catch(exception => setError(`[${exception.toString()}]`))
     }
-    render() {
-        let busss = <div className="bust">
-            No Tracked Vehicles to show. 
-            <br></br>
-            <span className='update' onClick={this.loadBusss}>Check again</span>, check your inputs, or check the schedule.
-        </div>
-        let stopName
-        if (this.state.buss[0]){
-            stopName = this.state.buss[0].MonitoredVehicleJourney.MonitoredCall.StopPointName
-            let key = 0 
-            busss = this.state.buss.map(bus => {
-                return <Bus bus={bus} key={key++} /> 
-            })   
-        }
-        return (
-            <div className="any-stop">
-                <div className="header">
-                    <Link to={`/anystop/${this.props.agency}/${this.props.stopCode}`}>
-                        <div className="map-link">
-                            <span title="bus stop" role="img" aria-label="bus stop emoji">🚏</span>                  
-                            { this.props.title 
-                            ? this.props.title
-                            : stopName
-                                ? stopName
-                                : `${this.props.agency}#${this.props.stopCode}` }
-                        </div>
-                    </Link> 
+    useEffect(loadBusss)
+
+    let busss = <div className="bust">
+                    No Tracked Vehicles to show. 
+                        <br></br>
+                    <span className='update' onClick={ () => {setError(''); loadBusss()} }>
+                        Check again
+                    </span>, check your inputs, or check the schedule. <span className='error'>{error}</span>
                 </div>
-                {busss}
-            </div>
-        );
+    let stopName
+    if (buss[0]) {
+        stopName = buss[0].MonitoredVehicleJourney.MonitoredCall.StopPointName
+        let key = 0 
+        busss = buss.map(bus => {
+            return <Bus bus={bus} key={key++} /> 
+        })   
     }
+    return (
+        <div className="any-stop">
+            <div className="header">
+                <Link to={`/anystop/${props.agency}/${props.stopCode}`}>
+                    <div className="map-link">
+                        <span title="bus stop" role="img" aria-label="bus stop emoji">🚏</span>                  
+                        { props.title 
+                        ? props.title
+                        : stopName
+                            ? stopName
+                            : `${props.agency}#${props.stopCode}` }
+                    </div>
+                </Link> 
+            </div>
+            {busss}
+        </div>
+    );
 }
 export default AnyStop;
